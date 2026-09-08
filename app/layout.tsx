036f91993c2d00d40116c30fac08be42685109f9
ahmadzamani72556-bit <ahmadzamani72556@gmail.com>
2026-09-08 10:41:07 +0800

    Show AI assistant in Music

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import AiAssistant from "../components/ai-assistant";
 
 export const metadata: Metadata = {
   title: "SALVIAN AI MUSIC",
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <body>{children}<AiAssistant /></body>
     </html>
   );
 }
